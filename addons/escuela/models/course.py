37514408a64e91314e35ce256e9c5a10ba92b993
Victor Hong <vhoncito@gmail.com>
2025-01-20 18:40:38 +0000

    Cambios en course, instructor y student

--- addons/escuela/models/course.py
+++ addons/escuela/models/course.py
@@ -2,11 +2,11 @@
 from odoo import models, fields
  
 class Course(models.Model):
-    _name = 'custom.course'
+    _name = 'escuela.course'
     _description = 'Course'
 
     name = fields.Char(string='Nombre', required=True)
     surname = fields.Text(string='Descripcion')
     schedule = fields.Datetime(string='Horario')
-    instructor_id = fields.Many2one('custom.instructor', string='Instructor')
-    student_ids = fields.Many2many('custom.student', string='Estudiantes')
+    instructor_id = fields.Many2one('escuela.instructor', string='Instructor')
+    student_ids = fields.Many2many('escuela.student', string='Estudiantes')
--- addons/escuela/models/student.py
+++ addons/escuela/models/student.py
@@ -1,11 +1,11 @@
 from odoo import models, fields
 
 class Student(models, Model):
-    _name = 'custom.student'
+    _name = 'escuela.student'
     _description = 'Student'
 
     name = fields.Char(string='Nombre', required=True)
     surname = fields.Char(string='Apellidos', required=True)
     phone = fields.Char(string='Telefono')
     email = fields.Char(string='Email')
-    course_ids = fields.Many2many('custom.course', string='Cursos')
+    course_ids = fields.Many2many('escuela.course', string='Cursos')
