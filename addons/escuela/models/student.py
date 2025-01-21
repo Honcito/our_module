9a990241d1e4c8e71a67be75dc62f2e73aee82c1
Victor Hong <vhoncito@gmail.com>
2025-01-21 18:03:36 +0000

    actualizando campo foto en modelo student

--- addons/escuela/models/student.py
+++ addons/escuela/models/student.py
@@ -8,6 +8,7 @@ class Student(models.Model):
     surname = fields.Char(string='Apellidos', required=True)
     phone = fields.Char(string='Telefono')
     email = fields.Char(string='Email')
+    foto = fields.Binary(string='Foto')
     course_ids = fields.Many2many('escuela.course', string='Cursos')
     course_names = fields.Char(string='Nombres de los Cursos', compute='_compute_course_names', store=True)
      
