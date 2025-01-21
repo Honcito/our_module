fce48279c127f6f6fc678636f166fc904b4aca02
Victor Hong <vhoncito@gmail.com>
2025-01-21 17:30:49 +0000

    Implementando foto Students

--- addons/escuela/models/student.py
+++ addons/escuela/models/student.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Student(models.Model):
     _name = 'escuela.student'
@@ -8,4 +8,6 @@ class Student(models.Model):
     surname = fields.Char(string='Apellidos', required=True)
     phone = fields.Char(string='Telefono')
     email = fields.Char(string='Email')
+    foto = fields.Binary(string='Foto')
     course_ids = fields.Many2many('escuela.course', string='Cursos')
+
